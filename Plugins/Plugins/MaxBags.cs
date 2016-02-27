cdec9cafae3de258737ffa89e487e719b0e6c67c
Stiffi <git@mxsd.de>
2016-02-27 13:34:06 +0100

    Initial commit of MaxBags plugin

---
+++ Plugins/Plugins/MaxBags.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("MaxBags", "Stiffi136", 0.2)]
+    public class MaxBags : RustPlugin
+    {
+        List<string> sleepDevices = new List<string>();
+
+        void LoadDefaultConfig() { }
+
+        private void CheckCfg<T>(string Key, ref T var)
+        {
+            if (Config[Key] is T)
+                var = (T)Config[Key];
+            else
+                Config[Key] = var;
+        }
+
+        static int MaximumBags = 1;
+
+        void Init()
+        {
+            CheckCfg("MaximumBags", ref MaximumBags);
+            SaveConfig();
+
+            sleepDevices.Add("assets/prefabs/deployable/sleeping bag/sleepingbag_leather_deployed.prefab (UnityEngine.GameObject)");
+            sleepDevices.Add("assets/prefabs/deployable/bed/bed_deployed.prefab (UnityEngine.GameObject)");
+        }
+
+        bool CheckMaxBags(ulong userid)
+        {
+            int count = 0;
+            foreach (SleepingBag bag in SleepingBag.FindForPlayer(userid, true))
+            {
+                count++;
+            }
+
+            if (count > MaximumBags)
+                return true;
+            else
+                return false;
+        }
+
+        private void OnEntityBuilt(Planner planner, GameObject gameobject)
+        {
+            if (planner.ownerPlayer == null) return;
+
+            if (sleepDevices.Exists(x => x == gameobject.ToString()))
+            {
+                bool check = CheckMaxBags(planner.ownerPlayer.userID);
+                if (check)
+                {
+                    gameobject.GetComponentInParent<BaseCombatEntity>().Kill(BaseNetworkable.DestroyMode.Gib);
+                    SendReply(planner.ownerPlayer, "Maximum number of sleeping bags or beds reached!");
+                    if (gameobject.ToString() == "assets/prefabs/deployable/sleeping bag/sleepingbag_leather_deployed.prefab (UnityEngine.GameObject)")
+                        planner.ownerPlayer.inventory.GiveItem(ItemManager.CreateByItemID(1253290621, 1));
+                    else
+                        planner.ownerPlayer.inventory.GiveItem(ItemManager.CreateByItemID(97409, 1));
+                }
+            }
+        }
+    }
+}
