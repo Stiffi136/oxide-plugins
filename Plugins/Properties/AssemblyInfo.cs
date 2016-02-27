fa2016fbd13208efa5c023f7f7c5f19de3d7ba1d
Stiffi <git@mxsd.de>
2016-02-27 13:23:38 +0100

    Renaming

--- Plugins/Properties/AssemblyInfo.cs
+++ Plugins/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Plugins")]
+[assembly: AssemblyTitle("Oxide.Plugins")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Plugins")]
+[assembly: AssemblyProduct("Oxide.Plugins")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
