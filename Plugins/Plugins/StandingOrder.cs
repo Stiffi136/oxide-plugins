40a80cf7c6487438a8ebfb1f999b753216359798
Stiffi <git@mxsd.de>
2016-02-29 11:27:32 +0100

    Added datafile system, reset timer

--- Plugins/Plugins/StandingOrder.cs
+++ Plugins/Plugins/StandingOrder.cs
@@ -1,10 +1,19 @@
 
+using Oxide.Core;
+using System;
+using System.Collections.Generic;
+
 namespace Oxide.Plugins
 {
     [Info("Standing Order", "Stiffi136", 0.1)]
     class StandingOrder : RustPlugin
     {
         private Timer secondTimer;
+
+
+        ////////////////////////////
+        // Config
+        ////////////////////////////
 
         void LoadDefaultConfig() { }
 
@@ -16,22 +25,44 @@ namespace Oxide.Plugins
                 Config[Key] = var;
         }
 
-        static string PayTime = "20:00:00";
+        static int PayTime = 20;
+        static int ResetTime = 8;
+
+
+        ////////////////////////////
+        // Data
+        ////////////////////////////
+
+        class StoredData
+        {
+            public bool wasExecuted;
+            public List<OrderInfo> Orders = new List<OrderInfo>();
+        }
+
+        class OrderInfo
+        {
+            public ulong FromPlayerID;
+            public ulong ToPlayerID;
+            public float Amount;
+            public int ExecutionsLeft;
+        }
+
+        StoredData storedData;
+
+
 
         void Init()
         {
-            CheckCfg("PayTime", ref PayTime);
+            // Config
+            CheckCfg("PayTimeHour", ref PayTime);
+            CheckCfg("ResetTimeHour", ref ResetTime);
             SaveConfig();
 
-            secondTimer = timer.Repeat(1, 0, () => CheckTime());
-        }
+            // Data
+            storedData = Interface.Oxide.DataFileSystem.ReadObject<StoredData>("StandingOrder");
 
-        void CheckTime()
-        {
-            if (System.DateTime.Now.ToString("HH:mm:ss") == PayTime)
-            {
-                Puts("Pay Time!");
-            }
+            // Timer
+            secondTimer = timer.Repeat(1, 0, () => CheckTime());
         }
 
         void Unloaded()
@@ -39,5 +70,68 @@ namespace Oxide.Plugins
             if (secondTimer != null)
                 secondTimer.Destroy();
         }
+
+        void SaveData()
+        {
+            Interface.Oxide.DataFileSystem.WriteObject("StandingOrder", storedData);
+        }
+
+        void CreateNewOrder(ulong fromPlayerID, ulong toPlayerID, float amount, int executions = -1)
+        {
+            var order = new OrderInfo();
+            order.FromPlayerID = fromPlayerID;
+            order.ToPlayerID = toPlayerID;
+            order.Amount = amount;
+            order.ExecutionsLeft = executions;
+
+            storedData.Orders.Add(order);
+        }
+
+        void ShowOrder(int index)
+        {
+            if (storedData.Orders.Count - 1 < index || index < 0)
+                return;
+            Puts(storedData.Orders[index].FromPlayerID.ToString());
+        }
+
+        void CheckTime()
+        {
+            if (DateTime.Now.Hour <= PayTime && !storedData.wasExecuted)
+            {
+                storedData.wasExecuted = true;
+                SaveData();
+                Puts("Pay Time!");
+            }
+
+            if (DateTime.Now.Hour <= ResetTime && storedData.wasExecuted)
+            {
+                storedData.wasExecuted = false;
+                SaveData();
+            }
+        }
+
+
+        ////////////////////////////
+        // Chat Commmands
+        ////////////////////////////
+
+        [ChatCommand("sotest")]
+        void cmdStandingOrderTest(BasePlayer player, string command, string[] args)
+        {
+            CreateNewOrder(player.userID, player.userID, 1000);
+            SaveData();
+        }
+
+        [ChatCommand("sotest2")]
+        void cmdStandingOrderTest2(BasePlayer player, string command, string[] args)
+        {
+            ShowOrder(Convert.ToInt32(args[0]));
+        }
+
+
+        ////////////////////////////
+        // Console Commands
+        ////////////////////////////
+
     }
 }
